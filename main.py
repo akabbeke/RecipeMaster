91c1ded38d6974516e8e9d6bf7fdb40a2819238a
Adam Kabbeke <adamkabbeke@v1507-wn-172-21-158-207.campus-dynamic.uwaterloo.ca>
2013-01-13 19:50:27 -0500

    sorta works

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ import numpy as np
 def NumpySearcher(nparray,value):
 	#Expects a 1d numpy array, returns index of the first instance of a variable.
 	count = 0
-	for element in nparray:
-		counter += 1
+	for element in nparray[:,0]:
 		if element == value:
 			return count
+		count += 1
 	return "Not Found"
 
 def IngredAdder(ingredList,recipList):
 	for ingred in recipList:
-		print 'LOL'
-		#index = NumpySearcher(nparray,value)
+		index = NumpySearcher(ingredList,ingred)
+		if type(index) == int:
+			ingredList[index][1] = str(int(ingredList[index][1])+1)
+		else:
+			foo = np.array(((ingred,1),))
+			ingredList = np.append(ingredList,foo,axis = 0)
+	return ingredList
 
-def SearchEncoder(url,pref,search):
+def QueryEncoder(url,pref,search):
 	if(isinstance(search,list)):
 		if url != '':
 			url += '&'
@@ -33,22 +38,30 @@ def SearchEncoder(url,pref,search):
 	return url
 
 
-def Showsome(searchfor):
-	query = SearchEncoder('','q',searchfor)
+def Showsome(name = '',ingred = ''):
+	query = ''
+	if name != '':
+		query = QueryEncoder(query,'q',name)
+	if ingred != '':
+		query = QueryEncoder(query,'i',ingred)
 	url = 'http://www.recipepuppy.com/api/?%s' % query
-	print url
 	searchResponse = urllib.urlopen(url)
 	searchResults = searchResponse.read()
 	results = json.loads(searchResults)
-	print results
 	results = results['results']
 	num = random.randint(0,len(results)-1)
 	recipe = results[num]
+	return recipe
+
+kwargList = ("beef","chicken","fish","vegitarian","egg","curry","pasta","french","burger","steak","pork","rice")
+ingredList = np.array((('',0),))
+for count in range(7):
+	index = random.randint(0,len(kwargList)-1)
+	recipe = Showsome(kwargList[index])
 	print recipe['title']
-	print type(recipe['ingredients'])
 	print recipe['href']
-	#[0]['ingredients']
-	#print ingred
+	print "/n"
+	ingredList = IngredAdder(ingredList,recipe['ingredients'].split(', '))
+print ingredList
 
-
-Showsome('lunch')
+	
